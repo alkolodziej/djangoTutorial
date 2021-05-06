807eae0671ab284a36391ed3981326cc86621168
GamePL7 <60829268+GamePL7@users.noreply.github.com>
2021-05-06 14:34:04 +0200

    Namespacing URL names

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,16 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Question
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
+from .models import Question
 # Create your views here.
 
 
 # ...
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
 
 
 def index(request):
@@ -22,10 +19,6 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
-
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
@@ -33,3 +26,8 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
